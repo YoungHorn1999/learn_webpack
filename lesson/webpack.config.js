997cfb88144d985f1a95cc2e0836a68f3b6e8086
YoungHorn1999 <1026344206@qq.com>
2020-10-09 17:50:46 +0800

    打包css样式

--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -3,17 +3,27 @@ const path = require('path')
 module.exports = {
     entry: './src/index.js',
     module: {
-        rules: [{
-            test: /\.(png|jpg|gif)$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: 'images/',
-                    limit: 204800
+        rules: [
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images/',
+                        limit: 204800
+                    }
                 }
+            },
+            {
+                test: /\.scss$/,
+                use:[
+                        'style-loader', 
+                        'css-loader', 
+                        'sass-loader',
+                    ]
             }
-        }]
+        ]
     },
     output: {
         filename: 'bundle.js',
