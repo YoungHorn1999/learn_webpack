612c200f9d69b3551560a9ef2b285b2d6ce17ec4
YoungHorn1999 <1026344206@qq.com>
2020-10-10 09:46:12 +0800

    使用plugin打包

--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -25,6 +27,11 @@ module.exports = {
             }
         ]
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'src/index.html'
+    }), new CleanWebpackPlugin({
+        cleanAfterEveryBuildPatterns: ['dist']
+    })],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
